e91b11af6bb226f71852bb5592b255f8a8697887
Benjamin Sabin <Bs542@kent.ac.uk>
2024-10-27 10:53:47 +0000

    list manip

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,20 @@
 ### Part Three -- your code goes here. 
+def main():
+
+    list = [3,1,4,1,5,9,2,6,5,3]
+
+    list.sort()
+
+    x = 0 
+    counter = list.count(1)
+
+    while x < counter:
+        x = x +1
+        list.pop(list.index(int(1))) 
+    
+    extra = [7,8]
+
+    list.extend(extra)
+    print(list)
+
+main()
